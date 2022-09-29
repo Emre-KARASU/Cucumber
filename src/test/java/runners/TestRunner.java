3b12812db35c01c7dad15ffe9e9a4f8cd787ecf2
EmreKARASU2546 <yargiiiic@gmail.com>
2022-09-29 17:14:38 +0300

    Cucumber Son

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "stepDefinitions",
         tags = "@aut",
-        dryRun = true
+        dryRun = false
         //tags = "@ikisi or @java", ikisini birlikte calistirmak icin kullanilir
 
 )
